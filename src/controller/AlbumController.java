e1469bd971508be9a25e73263c1c1b4aebdde448
Daoun Oh <daounski@gmail.com>
2016-04-05 22:53:11 -0400

    add album

--- src/controller/AlbumController.java
+++ src/controller/AlbumController.java
@@ -6,8 +6,13 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 public class AlbumController implements Initializable{
@@ -20,9 +25,22 @@ public class AlbumController implements Initializable{
 	public int numAlbum = 0;
 	
 	public void addAlbum(ActionEvent event){
-		Text scenetitle = new Text("Welcome");
-        scenetitle.setId("welcome-text");
-        albumList.add(scenetitle, 0, 0, 2, 1);
+		int row = numAlbum/3;
+        int col = numAlbum%3;
+        BorderPane album = new BorderPane();
+        
+        ImageView image = new ImageView("/view/no_photo.png");
+        image.setFitHeight(100);
+        image.setFitWidth(100);
+        
+        Text albumtitle = new Text("Album 1");
+        AnchorPane.setBottomAnchor(albumtitle, 10.0);
+        
+        album.setCenter(image);
+        album.setBottom(albumtitle);
+        BorderPane.setAlignment(albumtitle, Pos.CENTER);
+        
+        albumList.add(album, col, row);
 		
 		numAlbum++;
 	}
